731efeb04f2fa07ede9bef34a25980680aeb7f43
Paul Kiddle <p.kiddle@nhm.ac.uk>
2018-05-04 16:50:51 +0100

    Disable Scratchpad Team password reset - needs to be explicit opt-in

--- mysql/scratchpads-team.sql
+++ mysql/scratchpads-team.sql
@@ -1,2 +1,2 @@
-UPDATE `users` SET `pass`='$S$DlKfHKC6iOAoj3QnqVR0y7oOLFDfiz213nPQdeCNWqB8XuSrJPFk', `status`=1 WHERE `uid`=1;
+--UPDATE `users` SET `pass`='$S$DlKfHKC6iOAoj3QnqVR0y7oOLFDfiz213nPQdeCNWqB8XuSrJPFk', `status`=1 WHERE `uid`=1;
 update apachesolr_environment set url='http://solr:8983/solr/scratchpads2' where env_id='solr';
