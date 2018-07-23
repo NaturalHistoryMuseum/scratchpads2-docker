0a66f4fde90e5b6b3898577be63eb40d94f12336
Paul <p.kiddle@nhm.ac.uk>
2018-07-23 15:28:48 +0100

    Only remove modules if the database is defined

--- mysql/overrides.sh
+++ mysql/overrides.sh
@@ -20,17 +20,17 @@ if [ ! -e $FIRST_RUN ] ; then
       echo "Setting unsafe password for Scratchpad Team user"
       echo "UPDATE users SET pass='\$S\$DlKfHKC6iOAoj3QnqVR0y7oOLFDfiz213nPQdeCNWqB8XuSrJPFk', status=1 WHERE uid=1;" >> $OVERRIDES;
     fi
-  fi
 
-  # if remove_modules
-  echo $REMOVE_MODULES;
-  IFS=';' read -r -a modules_remove <<< "$REMOVE_MODULES"
-  for module in "${modules_remove[@]}"
-  do
-    echo $module;
-    echo "UPDATE system SET status=0 WHERE name=TRIM('$module');" >> $OVERRIDES;
-  done
-  echo "DELETE FROM cache_bootstrap WHERE cid='systemlist';" >> $OVERRIDES;
+    # if remove_modules
+    echo $REMOVE_MODULES;
+    IFS=';' read -r -a modules_remove <<< "$REMOVE_MODULES"
+    for module in "${modules_remove[@]}"
+    do
+      echo $module;
+      echo "UPDATE system SET status=0 WHERE name=TRIM('$module');" >> $OVERRIDES;
+    done
+    echo "DELETE FROM cache_bootstrap WHERE cid='systemlist';" >> $OVERRIDES;
+  fi
 else
   echo "Skipping setup"
 fi
